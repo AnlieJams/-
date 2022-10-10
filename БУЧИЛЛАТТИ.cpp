c710eb16dc58e8a7f5b787416e26c41582375f70
AnlieJams <98762736+AnlieJams@users.noreply.github.com>
2022-10-10 16:50:42 +0300

    Add files via upload

---
+++ БУЧИЛЛАТТИ.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int fibonacci(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    else if (number == 1)
+    {
+        return 1;
+    }
+
+
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
+
+
+int main()
+{
+    int number;
+    cin >> number;
+
+
+    cout << fibonacci(number);
+}
